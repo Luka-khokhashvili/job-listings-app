f9382a5bc1e9a29d8b232847f12e699c2031aad6
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-04 11:19:17 +0400

    Host job listing data on MockAPI.io, Integrate new API endpoint

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Job } from '../interfaces/job';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  apiUrl = 'http://localhost:3000/JobList';
+  localApiUrl = 'http://localhost:3000/JobList'; // Local API endpoint, for testing
+  publicApiUrl = 'https://683fece55b39a8039a561fb8.mockapi.io/api/v1/jobs'; // Hosted API endpoint for better maintainability and scalability
 
   constructor(private http: HttpClient) {}
 
   getAllJobData(): Observable<Job[]> {
-    return this.http.get<Job[]>(`${this.apiUrl}`);
+    return this.http.get<Job[]>(`${this.publicApiUrl}`);
   }
 
   getJobById(id: number): Observable<Job> {
-    return this.http.get<Job>(`${this.apiUrl}/${id}`);
+    return this.http.get<Job>(`${this.publicApiUrl}/${id}`);
   }
 }
