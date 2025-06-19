7c78c0acfad2cbf5d64dd9dd2f23d37b4a25e2aa
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-19 18:54:08 +0400

    Add minimal popup message for already selected filter tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataService } from './services/data.service';
-import { Job } from './interfaces/job';
 import { CommonModule } from '@angular/common';
 import { JobListComponent } from './components/job-list/job-list.component';
 import { FilterTagListComponent } from './components/filter-tag-list/filter-tag-list.component';
+import { TagFilterService } from './services/tag-filter.service';
 
 @Component({
   selector: 'app-root',
@@ -19,4 +18,20 @@ import { FilterTagListComponent } from './components/filter-tag-list/filter-tag-
 })
 export class AppComponent {
   title = 'job-listing-app';
+
+  constructor(private tagFilterService: TagFilterService) {}
+
+  public notificationMessage = '';
+  public showNotification = false;
+
+  ngOnInit() {
+    this.tagFilterService.notification$.subscribe((msg) => {
+      this.notificationMessage = msg;
+      this.showNotification = true;
+
+      setTimeout(() => {
+        this.showNotification = false;
+      }, 3000); // hide after 3s
+    });
+  }
 }
